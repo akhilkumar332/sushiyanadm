11b9e0d973d868f00820aeea033e0e33dccb9d3b
akhilkumar332 <akhilkumar332@gmail.com>
2025-03-23 18:35:52 +0100

    Removed unwanted functions from skripte.js

--- skripte/skripte.js
+++ skripte/skripte.js
@@ -1,23 +1,3 @@
-
-function toggleDescription(id) {
-    var description = document.getElementById(id);
-    if (description.style.display === "none" || description.style.display === "") {
-        description.style.display = "block";
-    } else {
-        description.style.display = "none";
-    }
-}
-
-function openModal(articleId) {
-    var modal = document.getElementById('myModal' + articleId);
-    modal.style.display = "block";
-}
-
-function closeModal(articleId) {
-    var modal = document.getElementById('myModal' + articleId);
-    modal.style.display = "none";
-    }
-
 // Schließen des Modalfensters, wenn der Benutzer außerhalb des Modalfensters klickt
 window.onclick = function(event) {
     var modals = document.getElementsByClassName('modal');
