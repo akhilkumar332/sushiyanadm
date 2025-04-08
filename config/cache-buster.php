b73539e9613db106fc8b114b7ac84b466d9f6961
akhilkumar332 <akhilkumar332@gmail.com>
2025-04-09 00:58:37 +0200

    Version Update 1.6

--- config/cache-buster.php
+++ config/cache-buster.php
@@ -50,7 +50,7 @@ if (file_exists($cacheFile) && (time() - filemtime($cacheFile)) < $cacheTTL) {
 }
 
 // Set version with reset key
-$resetKey = 'reset-v1.5';
+$resetKey = 'reset-v1.6';
 $version = ($latestMtime ?: time()) . '-' . $resetKey;
 
 // Make $version and $fileHash available to the including script
